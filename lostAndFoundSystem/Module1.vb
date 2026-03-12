ef59a115d014e55774e5eca8a78549d314a47c9d
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-12 18:00:21 +0200

    added login logic

---
+++ lostAndFoundSystem/Module1.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Data.SqlClient
+Module Module1
+    Public conn As New SqlConnection(My.Settings.dbConnectionString)
+    Public cmd As SqlCommand
+End Module
--- lostAndFoundSystem/My Project/Login.vb
+++ lostAndFoundSystem/My Project/Login.vb
@@ -1,5 +1,22 @@
 ﻿Public Class Login
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Try
+            conn.Open()
 
+            cmd = New SqlClient.SqlCommand($"SELECT COUNT(*) FROM users WHERE username=@usr AND password=@pwd;", conn)
+            cmd.Parameters.AddWithValue("@usr", Guna2TextBox1.Text)
+            cmd.Parameters.AddWithValue("@pwd", Guna2TextBox2.Text)
+            Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+            If count > 0 Then
+                MessageBox.Show("Logged In", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Wrong credentials", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
     End Sub
 End Class
